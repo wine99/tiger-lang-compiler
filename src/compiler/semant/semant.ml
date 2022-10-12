569ee4bf58ec3ec9a6080bb7b382f75044e95aed
jaschdoc <2072-jaschdoc@users.noreply.gitlab.au.dk>
2022-10-12 13:58:38 +0200

    Update tenv and collect refs

--- src/compiler/semant/semant.ml
+++ src/compiler/semant/semant.ml
@@ -520,10 +520,15 @@ and transDecl ({err; venv; tenv; break} as ctx : context) dec :
                 [] funcdecls ) )
       in
       (t_funcs, {err; venv= venv_funcs; tenv; break})
-  | A.TypeDec tydecs -> (
-    let tenv1 = List.fold_left (fun acc -> fun (A.Tdecl {name; _}) -> S.enter (tenv, name, Ty.NAME (name, ref None))) tenv tydecs in
-    raise NotImplemented
-  )
+  | A.TypeDec tydecs ->
+      let tenv', refs =
+        List.fold_left
+          (fun (tenv1, refs) (A.Tdecl {name; _}) ->
+            ( S.enter (tenv1, name, Ty.NAME (name, ref None))
+            , Ty.NAME (name, ref None) :: refs ) )
+          (tenv, []) tydecs
+      in
+      raise NotImplemented
   | _ -> raise NotImplemented
 
 and actual_type err pos = function
