1c446db86fa49dc7488efb122314ea527406988f
jaschdoc <2072-jaschdoc@users.noreply.gitlab.au.dk>
2022-11-08 16:28:38 +0100

    Refactor AccessVar

--- src/compiler/llcodegen/llcodegen.ml
+++ src/compiler/llcodegen/llcodegen.ml
@@ -218,10 +218,14 @@ and cgVar (ctxt : context) (H.Var {var_base; pos; ty}) =
   match var_base with
   | AccessVar (i, sym) -> (
     match i with
-    | 0 ->
-        let inst = Ll.Load (llvm_type, Ll.Id sym) in
+    | 0 -> (* Generalize later to n levels with gep *)
+      let locals_tpe = raise NotImplemented in (* Type of struct with local vars in llvm *)
+      let sumry_locls = Ll.Id ctxt.summary.locals_uid in
+      let offset = ctxt.summary.offset_of_symbol sym in
+        let load_locals_inst = gep_0 locals_tpe sumry_locls offset in
+        let inst = raise NotImplemented (* Ll.Load (llvm_type, ) *) in
         aiwf "tmp" inst
-    | n -> raise NotImplemented (* Generate instrs to traverse envs *) )
+    | n -> raise NotImplemented )
   | FieldVar (var, sym) -> raise NotImplemented
   | SubscriptVar (v, exp) ->
       let* cg_var = cgVar ctxt v in
